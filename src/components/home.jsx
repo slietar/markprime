b5ba8f45591eeb3b1975a1e839e6aaffd68d21e6
Simon Liétar <simon@lietar.net>
2022-06-04 19:43:00 +0200

    Redesign the recent files list

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -26,7 +26,7 @@ export default class Home extends React.Component {
 
   render() {
     return (
-      <div className="home-root"
+      <div className="home-root home-root--split"
         onDragEnter={(event) => {
           if ((event.target === event.currentTarget) && !event.currentTarget.contains(event.relatedTarget)) {
             console.log('drag enter', event.dataTransfer.files[0]);
@@ -46,100 +46,151 @@ export default class Home extends React.Component {
           }
         }}>
         <div className="home-center">
-          <div className="home-brand-root">
-            <div className="home-brand-logo">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><g><rect fill="none" height="24" width="24"/></g><g><polygon points="17.2,3 6.8,3 1.6,12 6.8,21 17.2,21 22.4,12"/></g></svg>
-            </div>
-            <div className="home-brand-title">Markprime</div>
-          </div>
-
-          <div className="home-content">
-            <p>Drop, select or paste a markdown file.</p>
-            <div className="home-buttons">
-              <button type="button" className="home-button" onClick={() => {
-                if (window.showOpenFilePicker) {
-                  pool.add(async () => {
-                    let handles = await util.wrapAbortable(window.showOpenFilePicker({
-                      multiple: true,
-                      types: [
-                        { description: 'Markdown or MDX files',
-                          accept: {
-                            'text/markdown': ['.md'],
-                            'text/mdx': ['.mdx']
-                          } },
-                      ]
-                    }));
-
-                    if (handles) {
-                      let backend = new FSAccessBackend(handles);
-                      this.props.onSelect(backend);
-                    }
-                  });
-                } else {
-                  let input = document.createElement('input');
-                  input.multiple = true;
-                  input.type = 'file';
-                  input.accept = '.md,.mdx,text/markdown,text/mdx';
-                  input.addEventListener('change', () => {
-                    let backend = new FilesBackend(input.files);
-                    this.props.onSelect(backend);
-                  });
-
-                  input.click();
-                }
-              }}>Select files</button>
-              {window.showDirectoryPicker && (
-                <button type="button" className="home-button" onClick={() => {
-                  pool.add(async () => {
-                    let handle = await util.wrapAbortable(window.showDirectoryPicker());
-
-                    if (handle) {
-                      let backend = new FSAccessBackend([handle]);
-                      this.props.onSelect(backend);
-                    }
-                  })
-                }}>Select directory</button>
-              )}
-              <button type="button" className="home-button home-button--github">
-                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" fill="currentColor" /></svg>
-                <div>Open GitHub gist</div>
-              </button>
-            </div>
-
-            {(this.props.recentFiles?.length > 0) && (
-              <div className="recent-root">
-                <div className="recent-header">
-                  <h2 className="recent-title">Recent files</h2>
-                </div>
-                <ul className="recent-list">
-                  {this.props.recentFiles
-                    .sort((a, b) => b.date - a.date)
-                    .map((info) => (
-                      <li className="recent-entry" key={info.id}>
-                        <button className="recent-name" onClick={() => {
-                          pool.add(async () => {
-                            let status = await info.handle.requestPermission();
-                            if (status === 'granted') {
-                              let backend = new FSAccessBackend([info.handle]);
-                              this.props.onSelect(backend);
-                            }
-                          });
-                        }}>
-                          <Icon name={info.handle.kind} />
-                          <span>{info.handle.name}</span>
-                        </button>
-                        <button className="recent-remove" onClick={() => {
-                          this.props.onRemoveRecentFile(info.id);
-                        }}>
-                          <Icon name="close" />
-                        </button>
-                      </li>
-                    ))
-                  }
-                </ul>
+          <div className="home-left">
+            <div className="home-brand-root">
+              <div className="home-brand-logo">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><g><rect fill="none" height="24" width="24" /></g><g><polygon points="17.2,3 6.8,3 1.6,12 6.8,21 17.2,21 22.4,12" /></g></svg>
               </div>
-            )}
+              <div className="home-brand-title">Markprime</div>
+            </div>
+
+            <div className="home-content">
+              <p>Drop, select or paste a markdown file.</p>
+              <div className="home-buttons">
+                <button type="button" className="home-button" onClick={() => {
+                  if (window.showOpenFilePicker) {
+                    pool.add(async () => {
+                      let handles = await util.wrapAbortable(window.showOpenFilePicker({
+                        multiple: true,
+                        types: [
+                          {
+                            description: 'Markdown or MDX files',
+                            accept: {
+                              'text/markdown': ['.md'],
+                              'text/mdx': ['.mdx']
+                            }
+                          },
+                        ]
+                      }));
+
+                      if (handles) {
+                        let backend = new FSAccessBackend(handles);
+                        this.props.onSelect(backend);
+                      }
+                    });
+                  } else {
+                    let input = document.createElement('input');
+                    input.multiple = true;
+                    input.type = 'file';
+                    input.accept = '.md,.mdx,text/markdown,text/mdx';
+                    input.addEventListener('change', () => {
+                      let backend = new FilesBackend(input.files);
+                      this.props.onSelect(backend);
+                    });
+
+                    input.click();
+                  }
+                }}>Select files</button>
+                {window.showDirectoryPicker && (
+                  <button type="button" className="home-button" onClick={() => {
+                    pool.add(async () => {
+                      let handle = await util.wrapAbortable(window.showDirectoryPicker());
+
+                      if (handle) {
+                        let backend = new FSAccessBackend([handle]);
+                        this.props.onSelect(backend);
+                      }
+                    })
+                  }}>Select directory</button>
+                )}
+                <button type="button" className="home-button home-button--github">
+                  <Icon name="github" />
+                  <div>Open GitHub gist</div>
+                </button>
+              </div>
+            </div>
           </div>
+
+          {(this.props.recentFiles?.length > 0) && (
+            <div className="home-right recent-root">
+              <input type="text" placeholder="Search for files" className="recent-input" />
+              <div className="recent-filters">
+                <button type="button" className="recent-filter">
+                  <Icon name="directory" />
+                  <div>Directories</div>
+                </button>
+                <button type="button" className="recent-filter">
+                  <Icon name="github" />
+                  <div>GitHub gists</div>
+                </button>
+              </div>
+              <div className="recent-list">
+                <div className="recent-entry-root">
+                  <button type="button" className="recent-entry-button">
+                    <Icon name="github" className="recent-entry-icon" />
+                    <div className="recent-entry-title">Some gist</div>
+                    <div className="recent-entry-subtitle">2 days ago</div>
+                  </button>
+                  <div className="recent-entry-actions">
+                    <button type="button" className="recent-entry-remove">
+                      <Icon name="close" />
+                    </button>
+                  </div>
+                </div>
+                <div className="recent-entry-root">
+                  <button type="button" className="recent-entry-button">
+                    <Icon name="directory" className="recent-entry-icon" />
+                    <div className="recent-entry-title">Very very very very very very very very very very very very very long</div>
+                    <div className="recent-entry-subtitle">2 days ago</div>
+                  </button>
+                  <div className="recent-entry-actions">
+                    <button type="button" className="recent-entry-remove">
+                      <Icon name="close" />
+                    </button>
+                  </div>
+                </div>
+                <div className="recent-entry-root">
+                  <button type="button" className="recent-entry-button">
+                    <Icon name="file" className="recent-entry-icon" />
+                    <div className="recent-entry-title">readme.md (+ 2 files)</div>
+                    <div className="recent-entry-subtitle">2 days ago</div>
+                  </button>
+                  <div className="recent-entry-actions">
+                    <button type="button" className="recent-entry-remove">
+                      <Icon name="close" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              {/* <ul className="recent-list">
+                {this.props.recentFiles
+                  .sort((a, b) => b.date - a.date)
+                  .map((info) => (
+                    <li className="recent-entry" key={info.id}>
+                      <button className="recent-name" onClick={() => {
+                        pool.add(async () => {
+                          let status = await info.handle.requestPermission();
+                          if (status === 'granted') {
+                            let backend = new FSAccessBackend([info.handle]);
+                            this.props.onSelect(backend);
+                          }
+                        });
+                      }}>
+                        <Icon name={info.handle.kind} />
+                        <span>{info.handle.name}</span>
+                      </button>
+                      <button className="recent-remove" onClick={() => {
+                        this.props.onRemoveRecentFile(info.id);
+                      }}>
+                        <Icon name="close" />
+                      </button>
+                    </li>
+                  ))
+                }
+              </ul> */}
+            </div>
+          )}
         </div>
       </div>
     );
--- src/components/icon.jsx
+++ src/components/icon.jsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 
 export default React.memo(function Icon(props) {
   return (
-    <svg className="icon">
+    <svg className={'icon ' + (props.className ?? '')}>
       {props.name && <use href={`#icon-${props.name}`} />}
     </svg>
   );
